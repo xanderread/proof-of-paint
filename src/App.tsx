d9b4388db8b1f1db8c5ddc1b33e376f9d8b0e93c
Sank6 <25014241+Sank6@users.noreply.github.com>
2024-10-26 13:22:09 +0100

    add in vis

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,29 @@
 import { useState } from 'react';
 import { backend } from './declarations/backend';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { authenticate } from './lib/authenticate';
 import { greet } from './lib/greet';
 
 function App() {
-  const [_greeted, setGreeted] = useState<string | null>(null);
-  const [_actor, setActor] = useState(backend);
+  const [state, setState] = useState('loading' as State);
+  const [greeted, setGreeted] = useState<string | null>(null);
+  const [actor, setActor] = useState(backend);
 
   authenticate().then((actor) => {
     if (actor) {
       setActor(actor);
       greet(actor, 'world').then((greeted) => {
         setGreeted(greeted);
+        setState('authenticated');
       });
     }
   });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p>State: {state}</p>
+      <p>Actor: {actor.toString()}</p>
+      <p>Greeted: {greeted}</p>  
     </>
   );
 }
---
+++ src/types/app.d.ts
@@ -0,0 +1,1 @@
+type State = 'loading' | 'authenticated' | 'unauthenticated';
