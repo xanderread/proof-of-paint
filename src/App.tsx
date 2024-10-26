dc1a7ffa91568028b5b901c2b64ab0280faa5868
Sank6 <25014241+Sank6@users.noreply.github.com>
2024-10-26 13:35:37 +0100

    add authentcation

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,30 @@ import { backend } from './declarations/backend';
 import './App.css';
 import { authenticate } from './lib/authenticate';
 import { greet } from './lib/greet';
+import { signin } from './lib/signin';
 
 function App() {
   const [state, setState] = useState('loading' as State);
   const [greeted, setGreeted] = useState<string | null>(null);
   const [actor, setActor] = useState(backend);
 
-  authenticate().then((actor) => {
-    if (actor) {
-      setActor(actor);
-      greet(actor, 'world').then((greeted) => {
-        setGreeted(greeted);
-        setState('authenticated');
-      });
-    }
+  signin().then(() => {
+    authenticate().then((actor) => {
+      if (actor) {
+        setActor(actor);
+        greet(actor, 'world').then((greeted) => {
+          setGreeted(greeted);
+          setState('authenticated');
+        });
+      }
+    });
   });
 
   return (
     <>
       <p>State: {state}</p>
       <p>Actor: {actor?.toString() || 'None'}</p>
-      <p>Greeted: {greeted}</p>  
+      <p>Greeted: {greeted}</p>
     </>
   );
 }
