3c8c50a6154b62de33f6b959936dadd0c717423b
Sank6 <25014241+Sank6@users.noreply.github.com>
2024-10-27 01:17:59 +0000

    modal

--- src/components/SetupArtist.tsx
+++ src/components/SetupArtist.tsx
@@ -1,6 +1,7 @@
+import '../styles/modal.css';
+
 import { useContext, useState } from 'react';
 import { UserContext } from '../App';
-import './SetupArtistModal.css';
 
 export default function SetupArtistModal() {
   const user = useContext(UserContext);
@@ -18,27 +19,26 @@ export default function SetupArtistModal() {
   };
 
   return (
-    <div id="modal" className="modal-overlay" style={{ display: 'none',  }}>
+    <div id="modal" className="modal-overlay" style={{ display: 'none' }}>
       <div className="modal-content">
-        <h2 className="modal-title">Enter Artist Details</h2>
+        <h2 className="modal-title">Sign up</h2>
         <div className="modal-body">
-          <label>
-            Alias:
-            <input type="text" value={alias} onChange={(e) => setAlias(e.target.value)} placeholder="Enter alias" />
-          </label>
-          <label>
-            Wallet Address:
-            <input
-              type="text"
-              value={walletAddress}
-              onChange={(e) => setWalletAddress(e.target.value)}
-              placeholder="Enter wallet address"
-            />
-          </label>
+          <input type="text" value={alias} onChange={(e) => setAlias(e.target.value)} placeholder="Alias" />
+
+          <input
+            type="text"
+            value={walletAddress}
+            onChange={(e) => setWalletAddress(e.target.value)}
+            placeholder="Wallet address"
+          />
         </div>
-        <div>
-          <button onClick={handleSave} className="modal-button">Save</button>
-          <button onClick={handleCancel} className="modal-button">Back</button>
+        <div className="modal-button-container">
+          <button onClick={handleCancel} className="modal-button cancel-btn">
+            Cancel
+          </button>
+          <button onClick={handleSave} className="modal-button">
+            Save
+          </button>
         </div>
       </div>
     </div>
